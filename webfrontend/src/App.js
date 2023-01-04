5af62f86184acd4092f618aa4a31005cb73a37f5
Jerin Andrews <jerinandrews125@gmail.com>
2023-01-03 23:18:36 -0600

    Contact Page
    contact setup page

--- webfrontend/src/App.js
+++ webfrontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from './components/NavBar';
 import HomePage from './pages/HomePage';
 import CarList from './pages/CarList'
+import Contact from './pages/Contact'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import { Parallax } from 'react-parallax';
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route element={<HomePage/>} path="/" exact/>
           <Route element={<CarList/>} path="/CarList" exact/>
+          <Route element={<Contact/>} path="/Contact" exact/>
         </Routes>
       </Router>
 
