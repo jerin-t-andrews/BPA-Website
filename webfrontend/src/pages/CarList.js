02bb6bd3d1e4db85e5815da8cb6846953738b075
Jerin Andrews <jerinandrews125@gmail.com>
2023-01-04 20:22:48 -0600

    CarList and NavBar
    Just toying with different menu options and navbar

--- webfrontend/src/pages/CarList.js
+++ webfrontend/src/pages/CarList.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import data_ from '../json/CarList.json';
 
 const CarList = () => {
     return (
-        <div>CarList</div>
+        <div className="ListPage">
+            <img src={data_[1].path} alt="im"/>
+            {/* <img src={require(data_[1].path)} alt="imr"></img> */}
+            <br/>
+            <br/>
+            <select>
+                {data_.map((car) => {
+                    return <option key={car.make} value={car.make}>{car.make}</option>
+                })}
+            </select>
+            <br/>
+            <select>
+            {data_.map((car) => {
+                    return <option key={car.year} value={car.year}> {car.year} </option>
+                })}
+            </select>
+        </div>
+
     )
 }
 
