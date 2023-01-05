63f2401d96b890ef52fd12dcbab194d4f075a7b8
Robert Tezock <tezock.robert@gmail.com>
2023-01-04 22:04:30 -0600

    working on car list

--- webfrontend/src/pages/CarList.js
+++ webfrontend/src/pages/CarList.js
@@ -4,21 +4,26 @@ import data_ from '../json/CarList.json';
 const CarList = () => {
     return (
         <div className="ListPage">
+            <div className='options'>
+                <div className='options-items'>
+                    <select>
+                        {data_.map((car) => {
+                            return <option key={car.make} value={car.make}>{car.make}</option>
+                        })}
+                    </select>
+                    <br/>
+                    <select>
+                    {data_.map((car) => {
+                            return <option key={car.year} value={car.year}> {car.year} </option>
+                        })}
+                    </select>
+                </div>
+            </div>
+            <br></br>
             <img src={data_[1].path} alt="im"/>
             {/* <img src={require(data_[1].path)} alt="imr"></img> */}
             <br/>
             <br/>
-            <select>
-                {data_.map((car) => {
-                    return <option key={car.make} value={car.make}>{car.make}</option>
-                })}
-            </select>
-            <br/>
-            <select>
-            {data_.map((car) => {
-                    return <option key={car.year} value={car.year}> {car.year} </option>
-                })}
-            </select>
         </div>
 
     )
